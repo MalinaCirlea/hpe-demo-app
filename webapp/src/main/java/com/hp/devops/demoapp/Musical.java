c776f3225b85d774bea7480665023c9771dec7dd
sa@nga <sa@nga>
2021-02-05 16:26:14 +0200

    vulnerabTest

--- webapp/src/main/java/com/hp/devops/demoapp/Musical.java
+++ webapp/src/main/java/com/hp/devops/demoapp/Musical.java
@@ -23,13 +23,17 @@ public class Musical {
     public int type8;
 
 
+    public int type9;
+
+
+
     public Musical() {
         System.out.println("not        important constructor");
     }
 
 
     public void setMusical() throws ExceptionInInitializerError {
-        throw new ExceptionInInitializerError("some customize   exception with the name of " + name  + age + type1 + type2 + type3 + type8);
+        throw new ExceptionInInitializerError("some customize   exception with the name of " +type9+ name  + age + type1 + type2 + type3 + type8);
     }
 
 }
